676de04f0a3c81565c11807b330fb5bfcca59af6
VIctor <colen.victor@icloud.com>
2021-09-19 21:19:40 -0300

    add TwoSum challenge

---
+++ leetcode-challenges/TwoSum.swift
@@ -0,0 +1,21 @@
+//
+//  TwoSum.swift
+//  leetcode-challenges
+//
+//  Created by Victor Colen on 19/09/21.
+//
+
+import Foundation
+
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        for i in 0..<nums.count {
+            for j in (i + 1) ..< nums.count {
+                if nums[i] + nums[j] == target {
+                    return [i, j]
+                }
+            }
+        }
+        return [0]
+    }
+}
