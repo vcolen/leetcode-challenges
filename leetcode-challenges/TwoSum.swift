d288827777f10b3ba1e92be1b6c02cc02bfd71c0
VIctor <colen.victor@icloud.com>
2021-09-19 21:27:35 -0300

    add twosum

--- leetcode-challenges/TwoSum.swift
+++ leetcode-challenges/TwoSum.swift
@@ -7,15 +7,16 @@
 
 import Foundation
 
-class Solution {
-    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-        for i in 0..<nums.count {
-            for j in (i + 1) ..< nums.count {
-                if nums[i] + nums[j] == target {
-                    return [i, j]
-                }
+
+func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    for i in 0..<nums.count {
+        for j in (i + 1) ..< nums.count {
+            if nums[i] + nums[j] == target {
+                return [i, j]
             }
         }
-        return [0]
     }
+    return [0]
 }
+
+
